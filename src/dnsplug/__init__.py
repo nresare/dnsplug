e87ccdf8ebdf5927fb27c3e45bbdc6def33a4d51
Noa Resare <noa@resare.com>
2018-04-04 19:07:31 +0100

    Add lookup() to top simple top level interface

---
+++ src/dnsplug/__init__.py
@@ -0,0 +1,37 @@
+# dnsplug - a thin wrapper and caching layer for dns lookups
+# Copyright (C) 2007-2016 Stuart Gathman <stuart@gathman.org>
+# Copyright (C) 2018 Noa Resare <noa@resare.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from .dns import Session
+from typing import Union, Tuple, List
+
+SESSION_SINGLETON = Session()
+
+ResultType = Union[str, Tuple[int, str]]
+
+
+def lookup(name, query_type):
+    # type: (str, str) -> List[ResultType]
+    """Lookup and return the DNS record with name and query_type. The result is
+    cached for improved performance.
+
+    :param name: a name, such as "google.com"
+    :param query_type: a dns record type, such as "A" or "TXT"
+    :return: a list of strings with the lookup result, or None if there was
+    no record.
+    """
+    return SESSION_SINGLETON.dns(name, query_type)
